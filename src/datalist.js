749c35c29cc09552b532b6f5bfee16c17f0a81e6
Shaheryar Abid <sharyar0310@gmail.com>
2023-01-19 17:14:24 +0500

    Clear the linters errors

--- src/datalist.js
+++ src/datalist.js
@@ -1,9 +1,9 @@
 // data list
 
 export default class DataList {
-    constructor(description, completed, index) {
-      this.description = description;
-      this.completed = completed;
-      this.index = index;
-    }
+  constructor(description, completed, index) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
   }
+}
